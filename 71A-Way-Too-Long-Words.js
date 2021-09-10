6bb027fbac33c6eedc30ea6243d0fb5266c31512
Nothing <wwwaap@gmail.com>
2021-09-10 15:16:59 +0800

    Update 71A-Way-Too-Long-Words.js

--- 71A-Way-Too-Long-Words.js
+++ 71A-Way-Too-Long-Words.js
@@ -20,3 +20,32 @@ rl.on('close', () => {
 function solution (str) {
   return str.length > 10 ? (str[0] + (str.length - 2) + str[str.length - 1]) : str 
 }
+
+// another
+
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+const arr = []
+rl.on('line', line => {
+  arr.push(line)
+})
+rl.on('close', () => {
+ const res = solution(arr.slice(1))
+ res.forEach(e => console.log(e))
+})
+
+
+function solution (arr) {
+  const res = []
+  for(let str of arr) {
+    if(str.length <= 10) res.push(str)
+    else {
+      const tmp = str[0] + (str.length - 2) + str[str.length - 1]
+      res.push(tmp)
+    }
+  }
+  return res
+}
